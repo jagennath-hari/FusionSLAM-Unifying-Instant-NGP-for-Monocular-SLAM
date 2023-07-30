33a413c03a28c664285356e7d350f3f76aadbd72
Jagennath Hari <110366406+jagennath-hari@users.noreply.github.com>
2023-07-30 01:46:33 -0700

    Add files via upload

---
+++ trace.py
@@ -0,0 +1,10 @@
+import torch
+import torchvision
+from demo_superpoint  import SuperPointNet
+model = SuperPointNet()
+model.load_state_dict(torch.load("superpoint_v1OG.pth"))
+model.eval()
+example = torch.rand(1, 1, 640, 480)
+traced_script_module = torch.jit.trace(model, example)
+traced_script_module.save("superpoint_v1.pt")
+
